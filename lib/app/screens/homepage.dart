496f0387d172924e1b2785dc202764afd840242c
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 02:41:26 +0600

    Added navigation from home to check support page

--- lib/app/screens/homepage.dart
+++ lib/app/screens/homepage.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:symphony_app/app/GlobalBackground.dart';
+import 'package:symphony_app/app/screens/support_screen.dart';
 
 import '../data/constants.dart';
 
@@ -58,47 +59,55 @@ class Homepage extends StatelessWidget {
   }
 
   Widget support(BuildContext context) {
-    return Container(
-      width: 344.w,
-      height: 128.h,
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(16.0),
-        image: DecorationImage(
-          image: AssetImage("assets/images/pattern.png"),
-          fit: BoxFit.cover,
-        )
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              SvgPicture.asset("assets/vectors/headphone_icon.svg"),
-              8.horizontalSpace,
-              Text(
-                "Need Help",
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => CheckSupportScreen()),
+        );
+      },
+      child: Container(
+        width: 344.w,
+        height: 128.h,
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(16.0),
+          image: DecorationImage(
+            image: AssetImage("assets/images/pattern.png"),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset("assets/vectors/headphone_icon.svg"),
+                8.horizontalSpace,
+                Text(
+                  "Need Help",
+                  style: GoogleFonts.urbanist(
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16, letterSpacing: .2, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                "Check Support",
                 style: GoogleFonts.urbanist(
-                  textStyle: TextStyle(color: Colors.white, fontSize: 16, letterSpacing: .2, fontWeight: FontWeight.w600),
+                  textStyle: TextStyle(color: Colors.black, fontSize: 16, letterSpacing: .2, fontWeight: FontWeight.w500),
                 ),
               ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Text(
-              "Check Support",
-              style: GoogleFonts.urbanist(
-                textStyle: TextStyle(color: Colors.black, fontSize: 16, letterSpacing: .2, fontWeight: FontWeight.w500),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
