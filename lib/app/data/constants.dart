1528e1e7740f113b1e66ebb3d720a7a9162f08d6
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 02:25:24 +0600

    Added trending items section in homepage

---
+++ lib/app/data/constants.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> trending_items = [
+  {
+    "name": "Mobiles",
+    "image": "assets/images/phone_image.png"
+  },
+  {
+    "name": "Watches",
+    "image": "assets/images/watch_image.png"
+  },
+  {
+    "name": "Accessories",
+    "image": "assets/images/usb_cable_image.png"
+  }
+];
--- lib/app/screens/homepage.dart
+++ lib/app/screens/homepage.dart
@@ -4,6 +4,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:symphony_app/app/GlobalBackground.dart';
 
+import '../data/constants.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
@@ -21,6 +23,8 @@ class Homepage extends StatelessWidget {
                 Image.asset("assets/images/hero_image.png"), // Hero section
                 24.verticalSpace,
                 support(context),
+                24.verticalSpace,
+                trending(),
               ],
             ),
           ),
@@ -51,12 +55,12 @@ class Homepage extends StatelessWidget {
       height: 128.h,
       padding: EdgeInsets.symmetric(horizontal: 16.0),
       decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(16.0),
-          image: DecorationImage(
-            image: AssetImage("assets/images/pattern.png"),
-            fit: BoxFit.cover,
-          )
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(16.0),
+        image: DecorationImage(
+          image: AssetImage("assets/images/pattern.png"),
+          fit: BoxFit.cover,
+        )
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -88,6 +92,60 @@ class Homepage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget trending() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Trending Items',
+          style: GoogleFonts.urbanist(
+            textStyle: TextStyle(color: Colors.black, fontSize: 18, letterSpacing: .2, fontWeight: FontWeight.w700),
+          ),
+        ),
+        16.verticalSpace,
+        SizedBox(
+          width: double.infinity,
+          height: 130.h,
+          child: ListView.separated(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (_, index) => Container(
+              width: 103.w,
+              height: 126.h,
+              padding: EdgeInsets.only(
+                top: 10.h,
+                left: 6.h,
+                right: 6.h,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0)
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    trending_items[index]["name"],
+                    style: GoogleFonts.urbanist(
+                      textStyle: TextStyle(color: Colors.black, fontSize: 16, letterSpacing: 0, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  4.verticalSpace,
+                  SizedBox(
+                    height: 90,
+                    child: Image.asset(trending_items[index]["image"], fit: BoxFit.contain)
+                  )
+                ],
+              ),
+            ),
+            separatorBuilder: (_, __) => 16.horizontalSpace,
+            itemCount: trending_items.length
+          ),
+        ),
+      ],
     );
   }
 }
