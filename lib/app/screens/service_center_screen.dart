5ecdbfefd24a47d8c9decb23a12641e97d5ee8b9
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 14:29:54 +0600

    Added appbar in service center page

--- lib/app/screens/service_center_screen.dart
+++ lib/app/screens/service_center_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:symphony_app/app/GlobalBackground.dart';
 
 class ServiceCenterScreen extends StatelessWidget {
@@ -8,6 +9,25 @@ class ServiceCenterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GlobalBackground(
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Service Center",
+            style: GoogleFonts.urbanist(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          titleSpacing: 0,
+        ),
         body: Center(
           child: Text("SERVICE CENTER"),
         ),
