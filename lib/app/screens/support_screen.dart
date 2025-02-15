89d078f21b60648f07d759798aa775a96254fe4d
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 10:47:37 +0600

    Added navigation in grid items

--- lib/app/screens/support_screen.dart
+++ lib/app/screens/support_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:symphony_app/app/GlobalBackground.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:symphony_app/app/screens/lso_screen.dart';
+import 'package:symphony_app/app/screens/service_center_screen.dart';
+import 'package:symphony_app/app/screens/specification_screen.dart';
 
 class CheckSupportScreen extends StatelessWidget {
   const CheckSupportScreen({super.key});
@@ -39,10 +42,10 @@ class CheckSupportScreen extends StatelessWidget {
           crossAxisSpacing: 16.0,
           children: [
             item(context, title: "Hotline", asset:  "assets/vectors/hotline${themeIsLight ? "" : "-dark"}.svg"),
-            item(context, title: "Service Center", asset:  "assets/vectors/service-center${themeIsLight ? "" : "-dark"}.svg"),
-            item(context, title: "LSO Query", asset:  "assets/vectors/lso${themeIsLight ? "" : "-dark"}.svg"),
+            item(context, title: "Service Center", asset:  "assets/vectors/service-center${themeIsLight ? "" : "-dark"}.svg", page: ServiceCenterScreen()),
+            item(context, title: "LSO Query", asset:  "assets/vectors/lso${themeIsLight ? "" : "-dark"}.svg", page: LsoScreen()),
             item(context, title: "Feedback", asset:  "assets/vectors/feedback${themeIsLight ? "" : "-dark"}.svg"),
-            item(context, title: "Specification", asset:  "assets/vectors/specification${themeIsLight ? "" : "-dark"}.svg"),
+            item(context, title: "Specification", asset:  "assets/vectors/specification${themeIsLight ? "" : "-dark"}.svg", page: SpecificationScreen()),
             item(context, title: "Book An Appointment", asset:  "assets/vectors/booking${themeIsLight ? "" : "-dark"}.svg"),
           ],
         ),
@@ -50,26 +53,34 @@ class CheckSupportScreen extends StatelessWidget {
     );
   }
 
-  Widget item(BuildContext context, {required String asset, required String title}) {
-    return Container(
-      height: 100,
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(16.0)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SvgPicture.asset(asset),
-          Text(
-            title,
-            style: GoogleFonts.urbanist(
-              textStyle: Theme.of(context).textTheme.labelMedium,
-            ),
-          )
-        ],
+  Widget item(BuildContext context, {required String asset, required String title, Widget? page}) {
+    return InkWell(
+      onTap: page == null ? null : () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+      child: Container(
+        height: 100,
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(16.0)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset(asset),
+            Text(
+              title,
+              style: GoogleFonts.urbanist(
+                textStyle: Theme.of(context).textTheme.labelMedium,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
