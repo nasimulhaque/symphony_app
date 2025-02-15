0e69ded795372b5738e24b25df26cd1977c9e991
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 13:43:38 +0600

    Added grid layout for specification content

--- lib/app/screens/specification_screen.dart
+++ lib/app/screens/specification_screen.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SpecificationScreen extends StatelessWidget {
-  SpecificationScreen({super.key});
 
-  final List<Map<String, dynamic>> items = [
-    {'width': 1, 'height': 1, 'color': Colors.red},
-    {'width': 2, 'height': 1, 'color': Colors.green},
-    {'width': 1, 'height': 2, 'color': Colors.blue},
-    {'width': 1, 'height': 1, 'color': Colors.orange},
-    {'width': 2, 'height': 2, 'color': Colors.purple},
-    {'width': 1, 'height': 1, 'color': Colors.yellow},
-  ];
+  const SpecificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -38,12 +32,19 @@ class SpecificationScreen extends StatelessWidget {
         ),
         titleSpacing: 0,
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            item1()
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 16.0,
+            bottom: 40.0
+          ),
+          child: Column(
+            children: [
+              item1(),
+              16.verticalSpace,
+              customGrid()
+            ],
+          ),
         ),
       ),
     );
@@ -111,6 +112,90 @@ class SpecificationScreen extends StatelessWidget {
               child: Image.asset("assets/images/android_icon.png")
             )
           )
+        ],
+      ),
+    );
+  }
+
+  Widget squareItem({required String icon, required String title, required String description}) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(16.0)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SvgPicture.asset(icon),
+          8.verticalSpace,
+          Text(
+            title,
+            style: GoogleFonts.urbanist(
+              textStyle: TextStyle(color: Colors.white54, fontSize: 12, letterSpacing: .2, fontWeight: FontWeight.w500),
+            ),
+          ),
+          2.verticalSpace,
+          Text(
+            description,
+            style: GoogleFonts.urbanist(
+              textStyle: TextStyle(color: Colors.white, fontSize: 14, letterSpacing: .2, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget customGrid() {
+    return Container(
+      color: Colors.black,
+      margin: EdgeInsets.symmetric(horizontal: 16.0),
+      child: StaggeredGrid.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        children: [
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/cpu.svg", title: "Processor", description: "MediaTek Helio G99 6nm 2.2GHz Octa Core"),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/folder-file-storage.svg", title: "Storage", description: "ROM 128GB"),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/square-arrow-diagonal.svg", title: "Display", description: '6.7" FHD+ AMOLED GG5 120Hz\nPunch Hole'),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/chip2.svg", title: "Ram", description: '16GB (8GB Expandable RAM)'),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/simcard-dual.svg", title: "Sim Card", description: 'Nano+Nano/Micro TF Card'),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 2,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/camera-01.svg", title: "Camera", description: '64MP + 2MP Macro Rear & 32MP Super Selfie with Display Flash'),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/cellular-network.svg", title: "Network", description: '4G, 3G, 2G'),
+          ),
+          StaggeredGridTile.count(
+            crossAxisCellCount: 1,
+            mainAxisCellCount: 1,
+            child: squareItem(icon: "assets/vectors/battery-full.svg", title: "Battery", description: '5000mAH'),
+          ),
         ],
       ),
     );
