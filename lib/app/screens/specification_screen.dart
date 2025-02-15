09b45f53bc8ff12621cb560bf9f01aa2a5082a90
Nasimul Haque <nasimulhaque.robin@gmail.com>
2025-02-15 10:56:54 +0600

    Added Specification page appbar

--- lib/app/screens/specification_screen.dart
+++ lib/app/screens/specification_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:symphony_app/app/GlobalBackground.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SpecificationScreen extends StatelessWidget {
   const SpecificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GlobalBackground(
-      child: Scaffold(
-        body: Center(
-          child: Text("SPECIFICATION"),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Specification",
+          style: GoogleFonts.urbanist(
+            textStyle: Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.white),
+          ),
         ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios, // Custom icon
+            color: Theme.of(context).colorScheme.secondary, // Custom color
+          ),
+          onPressed: () {
+            // Handle back button press
+            Navigator.of(context).pop();
+          },
+        ),
+        titleSpacing: 0,
       ),
+      body: Container(),
     );
   }
 }
